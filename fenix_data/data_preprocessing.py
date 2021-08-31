70d1371406439828a0dc32ed763c72e76afbd375
Michael <zyqing601@163.com>
2021-09-01 01:06:22 +0800

    Update data_preprocessing.py

--- fenix_data/data_preprocessing.py
+++ fenix_data/data_preprocessing.py
@@ -3,12 +3,12 @@ from operator import index
 import numpy as np
 import os
 import pandas as pd
+
 # %%
 
 pic_dir = r"E:\User_Michaels\My Projects\Python Project\motion-detection\Datasets\fenix falling data"
 fall_dir_list = [path for path in os.listdir(pic_dir)]
 
- 
 # %%
 csv_dir = r"E:\User_Michaels\My Projects\Python Project\motion-detection\fenix_data\csv"
 # %%
@@ -19,7 +19,7 @@ for folder in fall_dir_list:
     folder_path = os.path.join(pic_dir, folder)
     pic_name_list = []
     for pic in os.listdir(folder_path):
-        (filepath,tempfilename) = os.path.split(pic)
+        (filepath, tempfilename) = os.path.split(pic)
         (shortname, extension) = os.path.splitext(tempfilename)
         pic_name_list.append([shortname, extension])
     full_pic_name_list.append(pic_name_list)
@@ -33,7 +33,8 @@ for csv_name, pic_n_list in zip(csv_name_list, full_pic_name_list):
 # %%
 for csv_name, df in zip(csv_name_list, df_save_list):
     # f =pd.ExcelWriter(os.path.join(csv_dir, csv_name+".xlsx"))
-    path = os.path.join(csv_dir, csv_name+".csv")
-    df.to_csv(path_or_buf=path ,index=False)
-        
+    path = os.path.join(csv_dir, csv_name + ".csv")
+
+    df.to_csv(path_or_buf=path, index=False)
+
 # %%
